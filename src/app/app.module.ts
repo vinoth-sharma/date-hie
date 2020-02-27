5502b64bc580cd7190dd99b7786c87716f710853
vinoth sharma <vinoth.veeramani@mu-sigma.com>
2020-02-27 16:33:55 +0530

    material added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ColumnMappingComponent } from './column-mapping/column-mapping.component';
 import { FormatCrudComponent } from './format-crud/format-crud.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FormatCrudComponent } from './format-crud/format-crud.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
